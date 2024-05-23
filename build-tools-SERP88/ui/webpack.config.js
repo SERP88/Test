7a5e3f118b66ebc399269193397a1fba7c6cddc9
SERP88 <SERP88@users.noreply.github.com>
2024-05-23 11:39:46 +0300

    Add files via upload

---
+++ build-tools-SERP88/ui/webpack.config.js
@@ -0,0 +1,25 @@
+const HtmlWebpackPlugin = require ('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+	mode: "development",
+	entry: './src/index.js',
+	output: {
+		filename: 'app.js',
+		path: path.resolve(__dirname,'dist'),
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './src/index.html',
+	}),
+		new CopyWebpackPlugin({
+			patterns: [
+				{
+					from: './src/style.css',
+					to: 'css'
+				}
+			]
+		})
+	],
+};	
